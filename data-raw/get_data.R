ffb64b3adf878174f0a8832dca3e8b59241975ea
Lee-Anne Cecchini <leececchini@gmail.com>
2017-02-16 14:43:51 +0200

    wrote data to sql lite database

--- data-raw/get_data.R
+++ data-raw/get_data.R
@@ -1,8 +1,11 @@
 ## ---- makedb
 library(DBI)
 library(RSQLite)
-titanicdb<-dbConnect(SQLite(),dbname="../data-raw/titanic.sqlite")
+titanicdb <- dbConnect(SQLite(),dbname= "data-raw/titanic.sqlite")
 
 ## ---- writeTitanic
 library(PASWR)
-dbWriteTable(titanicdb, "titanic", titanic3, overwrite=TRUE)
+dbWriteTable(titanicdb, "titanic", titanic3, overwrite = TRUE)
+#dbWriteTable(database connection, name of table, data for table)
+#dbReadTable(database connection, name of table) to read the data
+
